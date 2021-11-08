67dd075b62c80ee0dac58849b3e10fa7c5548399
ephcoding <ephcoding@gmail.com>
2021-11-07 23:46:51 -0600

    add init class files

---
+++ _strategies/MomentumStrategy.js
@@ -0,0 +1,11 @@
+class MomentumStrategy {
+	// buy indicators
+	// pick ticker
+	// buy order
+	// holding period
+	// sell-for-profit indicators
+	// sell-for-loss indicators (stop loss)
+	// get-the-fuck-out indictors
+	// sell order
+	constructor() {}
+}
--- _strategies/arbitrage-1.js
+++
@@ -1,3 +0,0 @@
-class ArbitrageStrategy {
-  constructor() {}
-}
