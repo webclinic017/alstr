1f578a5fcdfa0332a11d11e0c70a2c05ff907874
ephcoding <ephcoding@gmail.com>
2021-11-08 21:52:33 -0600

    update import path

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv';
 dotenv.config();
-import { getAccount } from './_alpaca/account/getAccount.js';
+import { getAccount } from './src/data/alpaca/paper/getAccount.js';
 
 getAccount();
