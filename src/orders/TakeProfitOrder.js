c312551320444e67e753ac40959d3363173a1fbb
ephcoding <ephcoding@gmail.com>
2021-11-08 21:50:14 -0600

    add init order files

---
+++ src/orders/TakeProfitOrder.js
@@ -0,0 +1,12 @@
+class ProfitOrder {
+	// -- TICKER
+	// -- SIDE: buy/sell
+	// -- TYPE:
+	// market order: buy now @ retail
+	// *TIME sensitive / *NOT price sensitive
+	// limit order: buy at set $amount$
+	// *PRICE sensitive / *NOT time sensitive
+	// -- PRICE: (if it's a LIMIT order)
+	// -- QUANTITY: # of shares to buy
+	constructor() {}
+}
