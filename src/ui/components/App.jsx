e623ed76a18f452ed2b713a073ec3d3fc89d52a8
ephcoding <ephcoding@gmail.com>
2021-11-09 20:29:42 -0600

    add App styles

--- src/ui/components/App.jsx
+++ src/ui/components/App.jsx
@@ -1,16 +1,17 @@
 import CandleList from './CandleList.jsx';
 import CssBaseline from '@mui/material/CssBaseline';
+import '../scss/App.scss';
 
 export default function App() {
 	return (
-		<>
+		<div className='App'>
 			<CssBaseline />
 			<CandleList />
-		</>
+		</div>
 	);
 }
 
 // this get's rid of console's module.hot ERROR (from index.js in repo https://github.com/alexdevero/react-postcss-parcel-boilerplate/blob/master/src/index.js)
-// if (module.hot) {
-// 	module.hot.accept();
-// }
+if (module.hot) {
+	module.hot.accept();
+}
