d4c3b1cd4c5a1f74ab9cdc49a95a1e3eaff91115
ephcoding <ephcoding@gmail.com>
2021-11-09 20:30:24 -0600

    add CandlestickList styles

--- src/ui/components/CandleList.jsx
+++ src/ui/components/CandleList.jsx
@@ -2,45 +2,56 @@ import { useEffect, useState } from 'react';
 import { getBars } from '../../algo/alpaca/paper/getBars.js';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
+import '../scss/CandleList.scss';
 
 export default CandleList = () => {
 	const [candles, setCandles] = useState([]);
 
 	useEffect(async () => {
-		const candles = await getBars();
+		const barParams = {
+			ticker: 'GME',
+			start: '2020-12-31',
+			end: '2021-05-28',
+			timeframe: '1Day',
+		};
+		const candles = await getBars(barParams);
 		setCandles(candles);
 	}, []);
 
 	return (
-		<List>
-			{candles.map((candle) => (
-				// TODO: create util func that parses ints from candle time to use as comp key
-				<ListItem
-					key={candle.t
-						.toString()
-						.match(/[0-9]+/g)
-						.join('')}
-					candle={candle}
-					className='candle'
-				>
-					<div className='candle__metric'>
-						<p>OPEN</p>
-						<p>{candle.o.toFixed(2)}</p>
-					</div>
-					<div className='candle__metric'>
-						<p>HIGH</p>
-						<p>{candle.h.toFixed(2)}</p>
-					</div>
-					<div className='candle__metric'>
-						<p>LOW</p>
-						<p>{candle.l.toFixed(2)}</p>
-					</div>
-					<div className='candle__metric'>
-						<p>CLOSE</p>
-						<p>{candle.c.toFixed(2)}</p>
-					</div>
-				</ListItem>
-			))}
-		</List>
+		<>
+			<h1 className='candlesticks__header'>
+				<span className='candlesticks__header-label'>DATE</span>
+				<span className='candlesticks__header-label'>OPEN</span>
+				<span className='candlesticks__header-label'>HIGH</span>
+				<span className='candlesticks__header-label'>LOW</span>
+				<span className='candlesticks__header-label'>CLOSE</span>
+			</h1>
+			<List>
+				{candles.map((candle) => (
+					// TODO: create util func that parses ints from candle time to use as comp key
+					<ListItem
+						key={candle.t
+							.toString()
+							.match(/[0-9]+/g)
+							.join('')}
+						candle={candle}
+						className='candlestick'
+					>
+						<span className='candlestick__value'>
+							{new Date(candle.t).toLocaleDateString('en-US', {
+								weekday: 'long',
+								month: 'short',
+								day: 'numeric',
+							})}
+						</span>
+						<span className='candlestick__value'>${candle.o.toFixed(2)}</span>
+						<span className='candlestick__value'>${candle.h.toFixed(2)}</span>
+						<span className='candlestick__value'>${candle.l.toFixed(2)}</span>
+						<span className='candlestick__value'>${candle.c.toFixed(2)}</span>
+					</ListItem>
+				))}
+			</List>
+		</>
 	);
 };
