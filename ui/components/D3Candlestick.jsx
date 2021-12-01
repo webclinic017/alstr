a6614d34cbcaf6a9e11763e8b30ba9fad3d04878
ephcoding <ephcoding@gmail.com>
2021-12-01 14:30:21 -0600

    added getBarsInRange() to component for testing

--- ui/components/D3Candlestick.jsx
+++ ui/components/D3Candlestick.jsx
@@ -1,11 +1,12 @@
-import * as d3 from 'd3';
-import { useEffect } from 'react';
+import * as d3 from "d3";
+import { useEffect } from "react";
+import { getBarsInRange } from "../../algo/market_data/alpaca/getBarsInRange";
 
 const D3Candlestick = () => {
 	useEffect(() => {
 		const fakeData = [
 			{
-				t: '2021-02-01T16:01:00Z',
+				t: "2021-02-01T16:01:00Z",
 				o: 133.32,
 				h: 133.74,
 				l: 133.31,
@@ -15,23 +16,26 @@ const D3Candlestick = () => {
 		];
 
 		const svg = d3
-			.select('svg')
-			.attr('height', 450)
-			.attr('width', 450)
-			.style('background', '#333');
+			.select("svg")
+			.attr("height", 450)
+			.attr("width", 450)
+			.style("background", "#333");
 
 		svg
-			.selectAll('rect')
+			.selectAll("rect")
 			.data(fakeData)
 			.enter()
-			.append('rect')
-			.attr('x', 150)
-			.attr('y', 150)
-			.attr('height', (d, i) => {
+			.append("rect")
+			.attr("x", 150)
+			.attr("y", 150)
+			.attr("height", (d, i) => {
 				return (Math.max(d.o, d.c) - Math.min(d.o, d.c)) * 100;
 			})
-			.attr('width', 50)
-			.attr('fill', (d) => (d.c > d.o ? 'limegreen' : 'red'));
+			.attr("width", 50)
+			.attr("fill", d => (d.c > d.o ? "limegreen" : "red"));
+	});
+	useEffect(() => {
+		console.log(getBarsInRange());
 	});
 
 	return <svg id='candlestick'></svg>;
