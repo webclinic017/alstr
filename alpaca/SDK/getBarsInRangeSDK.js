e4bf338886c2d8d8ba8d25fe4790945f6205758e
ephcoding <ephcoding@gmail.com>
2021-12-01 17:21:59 -0600

    add separate SDK files for testing

---
+++ alpaca/SDK/getBarsInRangeSDK.js
@@ -0,0 +1,21 @@
+import { alpaca } from "./config";
+
+export const getBarsInRangeSDK = options => {
+	let bars;
+	try {
+		bars = alpaca.getBarsV2(options.ticker, {
+			limit: options.limit,
+			start: options.start,
+			end: options.end,
+			timeframe: options.timeframe,
+		});
+
+		return bars;
+	} catch (err) {
+		console.log(err);
+	}
+
+	// for await (let bar of bars) {
+	// 	console.log(bar);
+	// }
+};
