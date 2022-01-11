ac03fa96b15454355a3d693431617576e22768c5
Ephraim <admin@ephraimsmith.dev>
2022-01-11 16:05:10 -0600

    successfully fetch account info & bars using SDK methods

--- server/alpaca/paper_account/config.js
+++ server/alpaca/paper_account/config.js
@@ -1,8 +1,8 @@
 const Alpaca = require("@alpacahq/alpaca-trade-api");
 
 const alpaca = new Alpaca({
-	keyId: process.env.APCA_API_KEY_ID,
-	secretKey: process.env.APCA_API_SECRET_KEY,
+	keyId: process.env.APCA_KEY_PAPER,
+	secretKey: process.env.APCA_SECRET_PAPER,
 	paper: true,
 	usePolygon: false,
 });
--- server/server.js
+++ server/server.js
@@ -2,7 +2,28 @@ const alpaca = require("./alpaca/paper_account/config");
 const dotenv = require("dotenv");
 dotenv.config();
 
-(async () => {
+const getAccountInfo = async () => {
 	const res = await alpaca.getAccount();
-	console.log(">> ALPACA RESPONSE >>\n", res);
-})();
+	console.clear();
+	console.log(">> ACCOUNT >>\n", res);
+};
+
+const getBars = async () => {
+	const res = await alpaca.getBarsV2("HOG", {
+		limit: 1,
+		start: "2021-05-01",
+		end: "2021-05-31",
+		timeframe: "1Day",
+	});
+
+	console.clear();
+	console.log(">> DATA >>", res);
+
+	// for await (let b of res) {
+	// 	console.log(b);
+	// 	console.log(">> DATA >>", b);
+	// }
+};
+
+// getAccountInfo();
+getBars();
