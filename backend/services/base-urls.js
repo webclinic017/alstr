2914fb0268323a55c036281913e81a99c3ff9fff
Ephraim <admin@ephraimsmith.dev>
2022-01-30 11:54:38 -0600

    update env variable names

--- backend/services/base-urls.js
+++ backend/services/base-urls.js
@@ -1,7 +1,7 @@
 exports.APCA_LIVE_BASE_URL = "api.alpaca.markets";
 exports.APCA_PAPER_BASE_URL = "paper-api.alpaca.markets";
-exports.APCA_DATA_URL_STOCK = "https://data.alpaca.markets/v2";
-exports.APCA_DATA_URL_CRYPTO = "https://data.alpaca.markets/v1beta1/crypto";
-exports.APCA_STREAM_URL_STOCK = "wss://stream.data.alpaca.markets/v2/iex";
-exports.APCA_STREAM_URL_CRYPTO =
+exports.APCA_URL_DATA_STOCKS = "https://data.alpaca.markets/v2";
+exports.APCA_URL_DATA_CRYPTO = "https://data.alpaca.markets/v1beta1/crypto";
+exports.APCA_URL_STREAM_STOCK = "wss://stream.data.alpaca.markets/v2/iex";
+exports.APCA_URL_STREAM_CRYPTO =
 	"wss://stream.data.alpaca.markets/v1beta1/crypto";
