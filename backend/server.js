18b3ade5a4ff0b021eff9507f12144047b02a5f5
Ephraim <admin@ephraimsmith.dev>
2022-02-01 08:45:40 -0600

    update notes

--- backend/server.js
+++ backend/server.js
@@ -7,5 +7,14 @@ const alpacaAccount = async () => {
 	const account = await AlpacaApi.getAlpacaAccount();
 	console.log(account);
 };
+const minBars = async () => {
+	const bars = await AlpacaApi.getMinuteBars(
+		"HOG",
+		10,
+		"2021-12-01",
+		"2021-12-02"
+	);
+};
 
-alpacaAccount();
+// alpacaAccount();
+minBars();
--- backend/services/AlpacaService.ts
+++ backend/services/AlpacaService.ts
@@ -1,5 +1,6 @@
 let Alpaca = require("@alpacahq/alpaca-trade-api");
 const { key, secret, isPaper } = require("./config/paper_or_live");
+const Axios = require("axios");
 
 class AlpacaService {
 	constructor() {
@@ -10,12 +11,57 @@ class AlpacaService {
 		});
 	}
 
+	// ----------------------------------------------------------
+	// -- ACCOUNT
+	// ----------------------------------------------------------
 	async getAlpacaAccount() {
 		try {
 			const account = await this.alpaca.getAccount();
 			return account;
 		} catch (error) {
 			return error;
+		}
+	}
+
+	// ----------------------------------------------------------
+	// -- DATA
+	// ----------------------------------------------------------
+	async getMinuteBars(symbol, limit, start, end) {
+		let requestOptions = {
+			limit: limit,
+			start: start,
+			end: end,
+			timeframe: "1Min",
+		};
+		let axiosConfig = {
+			baseURL: "https://data.alpaca.markets",
+			url: `/v2/stocks/${symbol}/bars`,
+			params: {
+				start: start,
+				end: end,
+				limit: limit,
+				timeframe: "1Min",
+			},
+			// ,
+			// headers: {
+			// 	"APCA-API-KEY-ID": process.env.APCA_KEY_PAPER,
+			// 	"APCA-API-SECRET-KEY": process.env.APCA_SECRET_PAPER,
+			// },
+		};
+
+		try {
+			const minBars = await this.alpaca.getBarsV2(symbol, requestOptions);
+
+			// for await (let minBar of minBars) {
+			// 	console.log("MIN_BARS >>\n", minBar);
+			// }
+			console.log(minBars);
+			// const raw = await Axios(axiosConfig);
+			// const res = raw.data;
+
+			// console.log(raw.data);
+		} catch (error) {
+			console.log("ERROR >>\n", error);
 		}
 	}
 }
